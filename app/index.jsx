14c1a98b03f538eeb0a76c384791950d6dfe1b1f
FAIZY152 <fayazjani7576@gmail.com>
2025-09-19 10:47:45 +0500

    add-navigation-basic

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
@@ -10,6 +11,17 @@ export default function Index() {
           <Text className="text-2xl font-bold text-black text-center">
             Khan is here for match
           </Text>
+          <Link href={"/details"}>
+            {" "}
+            <Pressable className="bg-blue-600 px-6 py-3 rounded-xl">
+              <Text className="text-white font-medium">Details</Text>
+            </Pressable>
+          </Link>
+          <Link href={"/about"}>
+            <TouchableOpacity className="bg-pink-600 px-6 py-3 rounded-xl">
+              <Text className="text-white font-medium">About</Text>
+            </TouchableOpacity>
+          </Link>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
